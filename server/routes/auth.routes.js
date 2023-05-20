12bf44516e6df8a1975723b3c925658dd5d67125
Keberson <creedkiller.62@gmail.com>
2023-05-20 18:07:14 +0300

    keberson: Добавлена проверка на данные с фронта на авторизации и добавлена регистрация

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -9,6 +9,11 @@ const router = Router()
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body
+
+        if (email === null || password === null) {
+            return res.status(400).json({ message: `Incorrect data` })
+        }
+
         const candidate = await db.models.user.findOne({
             where: {
                 email: email
@@ -42,6 +47,44 @@ router.post('/login', async (req, res) => {
     }
 })
 
+router.post('/registration', async (req, res) => {
+    try {
+        const { username, email, password } = req.body
 
+        if (username === null || email === null || password === null) {
+            return res.status(400).json({ message: `Incorrect data` })
+        }
+
+        const candidate = await db.models.user.findOne({
+            where: {
+                email: email
+            },
+            attributes: [
+                'id_user'
+            ]
+        });
+
+        if (candidate !== null) {
+            return res.status(404).json({ message: `This email has already been registered` })
+        }
+
+        const user = await db.models.user.create({
+            user_name: username,
+            email: email,
+            password: password
+        });
+
+        const jwtToken = jwt.sign({ userID: user.id_user },
+            config.get('jwt'),
+            { expiresIn: '1h' }
+        );
+
+        return res.status(200).json({ token: jwtToken })
+    } catch (error) {
+        console.log(`Can't registration, error: ${error}`)
+
+        return res.status(500).json({ message: error })
+    }
+})
 
 module.exports = router
