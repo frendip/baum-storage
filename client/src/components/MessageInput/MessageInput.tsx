430ec49ac7b7c46c148876e17f6e52b7430789be
andreyivashchenko <99690245+andreyivashchenko@users.noreply.github.com>
2023-05-20 23:55:47 +0300

    andrey: создана начальная структура компонентов для страницы с чатами, добавлен приватный роут на страницу с чатами

---
+++ client/src/components/MessageInput/MessageInput.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './MessageInput.module.scss';
+
+const MessageInput = () => {
+  return <div className={classes.container}>input</div>;
+};
+
+export default MessageInput;
--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -2,13 +2,17 @@ import React, { JSX } from 'react';
 import { Navigate } from 'react-router-dom';
 import Login from '../pages/Auth/Login';
 import Registration from '../pages/Auth/Registration';
+import Chats from '../components/Chats/Chats';
 
 interface IRouter {
   path: string;
   element: JSX.Element;
 }
 
-export const privateRoutes: IRouter[] = [{ path: '/', element: <div>123</div> }];
+export const privateRoutes: IRouter[] = [
+  { path: '/', element: <Chats /> },
+  { path: '/:id', element: <Chats /> },
+];
 
 export const publicRoutes: IRouter[] = [
   { path: '/', element: <Navigate to={'/login'} /> },
