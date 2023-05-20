e6f7314af84ae767a99d39881ae77e467ba7413d
Keberson <creedkiller.62@gmail.com>
2023-05-20 18:38:43 +0300

    dev: удалена точка с запятой

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const PORT      = config.get('port') || 3000
 const app       = express()
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 app.use('/auth', require('./routes/auth.routes'))
 
 app.listen(PORT, () => {
