fc9da144d1c07a4ae24538614a69eabdf08c5395
Pavel Pavlov <ppavvvlov@gmail.com>
2023-05-21 14:00:00 +0300

    dev: работа над хуком userChat

--- client/src/hooks/useChat2.ts
+++ client/src/hooks/useChat2.ts
@@ -1,13 +1,30 @@
 import { baseUrl } from '../utills/cofig';
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IMessage, IUser } from '../types/types';
+import io from 'socket.io-client';
 
 const SERVER_URL = baseUrl;
 
 export const useChat = (room_id: number) => {
   const [users, setUsers] = useState<IUser[]>([]);
-
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const [socket] = useState(() => io(SERVER_URL, { query: { roomID: room_id } }));
 
-  const socketRef = useRef(null);
+  useEffect(() => {
+    socket.emit('user:add');
+    socket.on('users', (users) => {
+      setUsers(users);
+    });
+
+    socket.emit('message:get');
+    socket.on('messages', (messages) => {
+      setMessages(messages);
+    });
+    return () => {
+      // при размонтировании компонента выполняем отключение сокета
+      socket.disconnect();
+    };
+  }, []);
+
+  return { users, messages };
 };
