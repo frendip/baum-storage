9ee405eec21cb555d6b46967f97680ea90658732
Pavel Pavlov <ppavvvlov@gmail.com>
2023-05-21 10:32:10 +0300

    frendip: сделан компонент для кастомных инпутов, завершена работа на ChatsSidebar

--- client/src/components/ChatsSidebar/ChatsSidebar.tsx
+++ client/src/components/ChatsSidebar/ChatsSidebar.tsx
@@ -2,19 +2,41 @@ import React from 'react';
 import classes from './ChatsSidebar.module.scss';
 import Header from './Header/Header';
 import Item from './Item/Item';
+import { SearchInput } from '../UI/Input/Input';
 
 const ChatsSidebar = () => {
   return (
     <div className={classes.chatsSidebar}>
-      <Header />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+      <div className={classes.chatsSidebar__header}>
+        <Header />
+        <SearchInput />
+      </div>
+      <div className={classes.chatsSidebar__list}>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
     </div>
   );
 };
--- client/src/components/ChatsSidebar/Item/Item.tsx
+++ client/src/components/ChatsSidebar/Item/Item.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import classes from './Item.module.scss';
+import placeholder from '../../../assets/placeholder.png';
 
 const Item = () => {
-  return <div>123</div>;
+  return (
+    <div className={classes.item}>
+      <div className={classes.item__avatar}>
+        <img src={placeholder} alt="avatar" />
+      </div>
+      <div className={classes.item__info}>
+        <div className={classes.item__name}>Ivan Ivanov</div>
+        <div className={classes.item__message}>Lorem ipsum dolor sit.</div>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
