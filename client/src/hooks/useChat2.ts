147d1f79f57ced78c0f51200b2d14fc940519c0e
Pavel Pavlov <ppavvvlov@gmail.com>
2023-05-21 14:07:15 +0300

    dev: работа над хуком userChat x2

--- client/src/hooks/useChat2.ts
+++ client/src/hooks/useChat2.ts
@@ -2,16 +2,18 @@ import { baseUrl } from '../utills/cofig';
 import { useEffect, useState } from 'react';
 import { IMessage, IUser } from '../types/types';
 import io from 'socket.io-client';
+import { useAppSelector } from './useAppSelector';
 
 const SERVER_URL = baseUrl;
 
 export const useChat = (room_id: number) => {
+  const userId = useAppSelector((state) => state.user?.user?.id);
   const [users, setUsers] = useState<IUser[]>([]);
   const [messages, setMessages] = useState<IMessage[]>([]);
   const [socket] = useState(() => io(SERVER_URL, { query: { roomID: room_id } }));
 
   useEffect(() => {
-    socket.emit('user:add');
+    socket.emit('user:add', { userID: Number(userId) });
     socket.on('users', (users) => {
       setUsers(users);
     });
