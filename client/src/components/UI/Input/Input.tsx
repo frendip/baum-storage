17c0f6650afced93f44ccc2fc2b0148c07d5bc4a
Sushik_av_55b <alexghg9@mail.ru>
2023-05-21 12:37:57 +0300

    andrey: реализована верстка компонента input для чата

--- client/src/components/UI/Input/Input.tsx
+++ client/src/components/UI/Input/Input.tsx
@@ -1,13 +1,18 @@
-import React, { FC, HTMLAttributes } from 'react';
+import React, {FC, HTMLAttributes} from 'react';
 import clsx from 'clsx';
 import classes from './Input.module.scss';
 
-interface BaseInputProps extends HTMLAttributes<HTMLInputElement> {}
+interface BaseInputProps extends HTMLAttributes<HTMLInputElement> {
+}
 
-export const BaseInput: FC<BaseInputProps> = ({ className, ...props }) => {
-  return <input className={clsx(classes.baseInput, className)} {...props} />;
+export const BaseInput: FC<BaseInputProps> = ({className, ...props}) => {
+    return <input className={clsx(classes.baseInput, className)} {...props} />;
 };
 
-export const SearchInput: FC<BaseInputProps> = ({ className, ...props }) => {
-  return <BaseInput className={classes.searchInput} placeholder={'Search'} {...props} />;
+export const SearchInput: FC<BaseInputProps> = ({className, ...props}) => {
+    return <BaseInput className={classes.searchInput} placeholder={'Search'} {...props} />;
+};
+
+export const ChatInput: FC<BaseInputProps> = ({className, ...props}) => {
+    return <BaseInput className={classes.chatInput} placeholder={'Type a message...'} {...props} />;
 };
